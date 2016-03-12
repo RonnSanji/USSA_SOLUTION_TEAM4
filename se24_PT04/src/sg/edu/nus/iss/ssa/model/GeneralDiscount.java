5fef33fffd920497341ab1cc8aa9136d9ff5d646
chenghao1983 <joechenghao@gmail.com>
2016-03-12 14:39:19 +0800

    1. inherit GeneralDiscount from Entity 2. renamed attribute applicableTo to applicableInfo 3. added data/Discounts.dat

--- se24_PT04/src/sg/edu/nus/iss/ssa/model/GeneralDiscount.java
+++ se24_PT04/src/sg/edu/nus/iss/ssa/model/GeneralDiscount.java
@@ -1,10 +1,10 @@
 package sg.edu.nus.iss.ssa.model;
 
-public abstract class GeneralDiscount {
-	
-	 private String discountCode; 
-	 private String description;
-	 private int percentage;
-	 private char applicableTo;
+public abstract class GeneralDiscount extends Entity {
+
+	private String discountCode;
+	private String description;
+	private int percentage;
+	private char applicableInfo;
 
 }
