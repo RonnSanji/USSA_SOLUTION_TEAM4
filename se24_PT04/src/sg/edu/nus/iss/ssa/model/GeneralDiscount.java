69c7a00c7f725542e2a52819e817b909af145584
amarjeet298 <amarjeet2982@gmail.com>
2016-03-12 17:10:01 +0800

    Amar - test

--- se24_PT04/src/sg/edu/nus/iss/ssa/model/GeneralDiscount.java
+++ se24_PT04/src/sg/edu/nus/iss/ssa/model/GeneralDiscount.java
@@ -2,9 +2,11 @@ package sg.edu.nus.iss.ssa.model;
 
 public abstract class GeneralDiscount extends Entity {
 
-	private String discountCode;
-	private String description;
-	private int percentage;
-	private char applicableInfo;
+	protected String fileName = "Discounts.dat";
+	protected String className = "sg.edu.nus.iss.ssa.model.GeneralDiscount";
+	protected String[] properties = {"discountCode","discountDesc", "productDesc","quantity", "price",
+			"barCode","thresholdQuantity","orderQuantity"};
+	protected String mapKey = "barCode";
+
 
 }
