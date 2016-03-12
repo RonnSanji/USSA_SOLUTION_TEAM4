10ccd5dbf726d83dafff24412e83edda0a8b586e
ankur <jainankur020@gmail.com>
2016-03-12 16:03:06 +0800

    Store Keeper making it compatible to use reflection

--- se24_PT04/src/sg/edu/nus/iss/ssa/model/StoreKeeper.java
+++ se24_PT04/src/sg/edu/nus/iss/ssa/model/StoreKeeper.java
@@ -1,6 +1,12 @@
 package sg.edu.nus.iss.ssa.model;
 
 public class StoreKeeper extends Entity {
+
+	protected String fileName = "Storekeepers.dat";
+	protected String className = "sg.edu.nus.iss.ssa.model.StoreKeeper";
+	protected String[] properties = {"skName","skPassword"};
+	protected String mapKey = "skName";
+
 	private String skName;
 	private String skPassword;
 }
