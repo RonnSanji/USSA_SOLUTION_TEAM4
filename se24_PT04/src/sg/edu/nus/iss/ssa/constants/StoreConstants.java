056946831bf7c7a557a4644a1f153407c93c14a6
chenghao1983 <joechenghao@gmail.com>
2016-03-13 21:59:42 +0800

    1. add CATEGORY_ID_MAX_LENGTH

--- se24_PT04/src/sg/edu/nus/iss/ssa/constants/StoreConstants.java
+++ se24_PT04/src/sg/edu/nus/iss/ssa/constants/StoreConstants.java
@@ -1,10 +1,8 @@
 package sg.edu.nus.iss.ssa.constants;
 
-
 public class StoreConstants {
-	
-	public static final String FIELD_DELIMITER = ",";
-	public static final int CASH_EQ_POINTS =100;
-	
 
+	public static final String FIELD_DELIMITER = ",";
+	public static final int CASH_EQ_POINTS = 100;
+	public static final int CATEGORY_ID_MAX_LENGTH = 3;
 }
