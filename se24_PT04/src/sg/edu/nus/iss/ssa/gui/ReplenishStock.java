4911b855f696bf5c6a9015f384c799358530868a
chenghao1983 <joechenghao@gmail.com>
2016-03-16 23:45:14 +0800

    1. updated ReplenishStock

--- se24_PT04/src/sg/edu/nus/iss/ssa/gui/ReplenishStock.java
+++ se24_PT04/src/sg/edu/nus/iss/ssa/gui/ReplenishStock.java
@@ -1,9 +1,13 @@
 package sg.edu.nus.iss.ssa.gui;
 
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.LayoutManager;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import sg.edu.nus.iss.ssa.bo.FileDataWrapper;
@@ -21,6 +25,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.BevelBorder;
 
 public class ReplenishStock extends JFrame {
 
@@ -29,16 +36,19 @@ public class ReplenishStock extends JFrame {
 	 */
 	private static final long serialVersionUID = 2219926673801562401L;
 	private JPanel contentPane;
+	private JPanel panelResult;
 	private JTextField txtSearchText;
 	private JTable TbResult;
-	JComboBox comboBoxSearchBy;
+	JComboBox<String> comboBoxSearchBy;
+	JScrollPane scrollPane;
+
 	private String[] comboBoxSearchByItem = new String[] { "Name", "Description" };
 
 	private String searchBy;
 	private String searchText;
 	private Product selectedProduct;
 	private Collection<Product> productList;
-	private Collection<Product> productListResult;
+	private List<Product> productListResult;
 
 	/**
 	 * Create the frame.
@@ -61,7 +71,6 @@ public class ReplenishStock extends JFrame {
 		JButton btnSearch = new JButton("Search");
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO call method
 				search();
 			}
 		});
@@ -82,13 +91,34 @@ public class ReplenishStock extends JFrame {
 		comboBoxSearchBy.setBounds(122, 49, 104, 20);
 		contentPane.add(comboBoxSearchBy);
 
-		TbResult = new JTable();
+		panelResult = new JPanel();
+		panelResult.setBounds(41, 109, 534, 291);
+		contentPane.add(panelResult);
+
+		String[] columns = new String[] { "Product Name", "Product Description", "Price", "Current Quantity",
+				"Reorder Threshold", "Reorder Quantity" };
+
+		String[][] data = new String[2][];
+		data[0] = new String[] { "Test name 1", "Test Description 2", "100", "20", "20", "40" };
+		data[1] = new String[] { "Test name 2", "Test Description 2", "200", "22", "22", "20" };
+
+		TbResult = new JTable(data, columns);
+
 		TbResult.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		TbResult.setBounds(86, 367, 433, -220);
-		contentPane.add(TbResult);
+		//TbResult.setBounds(83, 396, 447, -249);
+		TbResult.setFillsViewportHeight(true);
+		// panelResult.add(TbResult);
+
+		JScrollPane scrollPane = new JScrollPane(TbResult);
+		scrollPane.setViewportBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		scrollPane.setLayout(null);
+		scrollPane.setBounds(83, 396, 447, -249);
+		panelResult.add(scrollPane);
+
+		scrollPane.setViewportView(TbResult);
 
 		JLabel lblNoResult = new JLabel("");
-		lblNoResult.setBounds(83, 109, 46, 14);
+		//lblNoResult.setBounds(83, 109, 46, 14);
 		contentPane.add(lblNoResult);
 
 		JButton btnReplenish = new JButton("Replenish");
@@ -98,7 +128,7 @@ public class ReplenishStock extends JFrame {
 
 			}
 		});
-		btnReplenish.setBounds(122, 456, 89, 23);
+		btnReplenish.setBounds(137, 456, 104, 23);
 		contentPane.add(btnReplenish);
 
 		JButton btnClose = new JButton("Close");
@@ -107,30 +137,45 @@ public class ReplenishStock extends JFrame {
 				dispose();
 			}
 		});
-		btnClose.setBounds(404, 456, 89, 23);
+		btnClose.setBounds(377, 456, 95, 23);
 		contentPane.add(btnClose);
+
 	}
 
 	private void search() {
 		productList = (Collection<Product>) FileDataWrapper.productMap.values();
-
+		productListResult = new ArrayList<Product>();
 		if (comboBoxSearchBy.getSelectedItem() != null) {
 			searchBy = comboBoxSearchBy.getSelectedItem().toString();
 			for (Product product : productList) {
 				// Name
 				if (searchBy == comboBoxSearchByItem[0]) {
 					if (product.getProductName().toUpperCase().contains(txtSearchText.getText().toUpperCase())) {
-						System.out.println(product.getProductName());
+						productListResult.add(product);
 					}
 				}
 				// Description
 				else if (searchBy == comboBoxSearchByItem[1]) {
 					if (product.getProductDesc().toUpperCase().contains(txtSearchText.getText().toUpperCase())) {
-						System.out.println(product.getProductDesc());
+						productListResult.add(product);
 					}
 				}
 
 			}
+			String[] columns = new String[] { "Product Name", "Product Description", "Price", "Current Quantity",
+					"Reorder Threshold", "Reorder Quantity" };
+			Object[][] data = new Object[productListResult.size()][];
+			for (int i = 0; i < productListResult.size(); i++) {
+				String[] values = new String[columns.length];
+				values[0] = productListResult.get(i).getProductName();
+				values[1] = productListResult.get(i).getProductDesc();
+				values[2] = String.valueOf(productListResult.get(i).getPrice());
+				values[3] = String.valueOf(productListResult.get(i).getQuantity());
+				values[4] = String.valueOf(productListResult.get(i).getThresholdQuantity());
+				values[5] = String.valueOf(productListResult.get(i).getOrderQuantity());
+				data[i] = values;
+			}
+
 		}
 
 	}
@@ -138,5 +183,4 @@ public class ReplenishStock extends JFrame {
 	private void replenish() {
 
 	}
-
 }
