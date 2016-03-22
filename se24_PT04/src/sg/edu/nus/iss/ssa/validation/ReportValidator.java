2e05eaff6892eb0dcbcbd93c70fed1076adfa383
gongsl <vinogong@gmail.com>
2016-03-22 07:18:44 -0700

    shengliang- add ReportValidator.java

---
+++ se24_PT04/src/sg/edu/nus/iss/ssa/validation/ReportValidator.java
@@ -0,0 +1,22 @@
+package sg.edu.nus.iss.ssa.validation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import sg.edu.nus.iss.ssa.constants.StoreConstants;
+
+public class ReportValidator {
+
+	public boolean isDateValid(String date) {
+	    if (date == null || !date.matches("\\d{4}-[01]\\d-[0-3]\\d"))
+	    	return false;	        
+	    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	    df.setLenient(false);
+	    try {
+	        df.parse(date);
+	        return true;
+	    } catch (ParseException ex) {
+	        return false;
+	    }
+	}
+}
