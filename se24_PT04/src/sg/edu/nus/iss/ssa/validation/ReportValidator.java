e93917639c342d517a3b52ca35eec9a1766db856
gongsl <vinogong@gmail.com>
2016-04-07 08:15:10 -0700

    update ReportValidator

--- se24_PT04/src/sg/edu/nus/iss/ssa/validation/ReportValidator.java
+++ se24_PT04/src/sg/edu/nus/iss/ssa/validation/ReportValidator.java
@@ -8,18 +8,21 @@ import sg.edu.nus.iss.ssa.constants.StoreConstants;
 public class ReportValidator {
 
 	public boolean isDateValid(String date) {
-		if(date.isEmpty()){
-			return true;
-		}
-		else if (!date.matches("\\d{4}-[01]\\d-[0-3]\\d"))
-	    	return false;	        
-	    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-	    df.setLenient(false);
-	    try {
-	        df.parse(date);
-	        return true;
-	    } catch (ParseException ex) {
-	        return false;
-	    }
+		// if(date.isEmpty()){
+		// return true;
+		// }
+		// else if (!date.matches("\\d{4}-[01]\\d-[0-3]\\d"))
+		// return false;
+		// SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		// df.setLenient(false);
+		// try {
+		// df.parse(date);
+		// return true;
+		// } catch (ParseException ex) {
+		// return false;
+		// }
+		if (date == null)
+			return false;
+		return true;
 	}
 }
